9489300c43fc8cd4501b9e42bd53e20c6c4dab36
Javier-peralta <javierin_jpr@hotmail.com>
2020-10-13 23:37:16 -0500

    pila

---
+++ pila.py
@@ -0,0 +1,12 @@
+pila=[3,4,5] # pila de inicio con un arreglo
+print (pila) #impresion de la pila
+#345
+
+pila.append(7)
+pila.append(8)
+
+print (pila)
+
+print ("saco este elemento", pila.pop())
+
+print("Quedo asi la pila", pila)
